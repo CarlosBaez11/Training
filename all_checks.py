6d32bf3ff5fbad38e5f82aed737c5775f52d51c9
CarlosBaez11 <70594075+CarlosBaez11@users.noreply.github.com>
2023-06-14 10:37:48 -0500

    Update all_checks.py
    Some code lines were ordered

--- all_checks.py
+++ all_checks.py
@@ -5,9 +5,11 @@ import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
-    return os.path.exist('/run/reboot-required'
-            )
+    return os.path.exist('/run/reboot-required')
+
+
 def main():
+    
     if check_reboot():
         print('Pending Reboot.')
         sys.exit(1)
